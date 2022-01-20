7b51505cd5dc6d2e26af05c1cc610275d9d3129f
Cristiano Sales <cristianosalesneves@gmail.com>
2022-01-20 13:00:20 -0300

    helpers: fetchItem.js

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,13 @@
-const fetchItem = () => {
-  
+const fetchItem = async (productId) => {
+  const url = `https://api.mercadolibre.com/items/${productId}`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    return error;
+  }
 };
 
 if (typeof module !== 'undefined') {
