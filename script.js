11cf383a41ef5deaa299bdcd733525c363c2f76a
Cristiano Sales <cristianosalesneves@gmail.com>
2022-01-20 13:02:10 -0300

    2. Adicione o produto ao carrinho de compras

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const classeItems = document.querySelector('.items');
+const classeCartItems = document.querySelector('.cart__items');
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -25,15 +28,15 @@ function createProductItemElement({ sku, name, image }) {
 }
 
 function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
+  return item.querySelector('span.item__sku').innerText; // retorna o sku do produto, exemplo MLB2025368569 
 }
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
+function createCartItemElement({ sku, name, salePrice }) {  
+  const li = document.createElement('li'); 
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
@@ -42,20 +45,33 @@ function createCartItemElement({ sku, name, salePrice }) {
 
 const computers = async () => {
   const apiComputers = await fetchProducts();
-  await apiComputers.forEach(({ id, title, thumbnail }) => {
-    const sku = id;
-    const name = title;
-    const image = thumbnail;
-    document.getElementsByClassName('items')[0]
-      .appendChild(createProductItemElement({ sku, name, image }));
+  await apiComputers.forEach(({ id: sku, title: name, thumbnail: image }) => {
+    classeItems.appendChild(createProductItemElement({ sku, name, image }));
+  });
+};
+
+const adicionaAoCarrinho = () => {
+  classeItems.addEventListener('click', async (event) => { // Ao clicar nesse botão você deve realizar uma requisição que irá retornar todos os dados específicos de um produto.
+    console.log(event.target);
+    if (event.target.className === 'item__add') { //  Verifica se clicou exatamente em um botão de class__add 
+      const produtoClicado = getSkuFromProductItem(event.target.parentNode); // recupera o parentNode do alvo, cujo contém sku, title, image e button
+      const { id: sku, title: name, price: salePrice } = await fetchItem(produtoClicado); // faz a requisição apenas do produto clicado
+      const listaClicado = createCartItemElement({ sku, name, salePrice }); // cria lista que irá para o carrinho
+      classeCartItems.appendChild(listaClicado); // O carrinho  recebe o a li inner text do produto clicado
+      const carrinho = classeCartItems.innerHTML; // acessa o innerHTML do carrinho
+      saveCartItems(carrinho); // a função dada em helpers salva o carrinho no Local Storage
+      //  totalPrice();   
+    }
   });
 };
 
 window.onload = async () => {
   await computers();
+  adicionaAoCarrinho();
 };
 
 //  References:
+// Douglas Marçal
 // Wendy Silva
 // Adriano Santos
 // Leandro de Oliveira
